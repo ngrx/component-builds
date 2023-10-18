5e09363ad952b2aec9b1a3a39630a96876edc102
GitHub <noreply@github.com>
2023-10-18 18:35:10 +0000

    3d250473 feat: upgrade Angular dependencies to v17 pre-release versions (#4068)

--- esm2022/src/push/push.module.mjs
+++ esm2022/src/push/push.module.mjs
@@ -5,15 +5,15 @@ import * as i0 from "@angular/core";
  * @deprecated This module is deprecated in favor of the standalone `PushPipe`.
  */
 export class PushModule {
-    /** @nocollapse */ static { this.ɵfac = i0.ɵɵngDeclareFactory({ minVersion: "12.0.0", version: "16.2.3", ngImport: i0, type: PushModule, deps: [], target: i0.ɵɵFactoryTarget.NgModule }); }
-    /** @nocollapse */ static { this.ɵmod = i0.ɵɵngDeclareNgModule({ minVersion: "14.0.0", version: "16.2.3", ngImport: i0, type: PushModule, imports: [PushPipe], exports: [PushPipe] }); }
-    /** @nocollapse */ static { this.ɵinj = i0.ɵɵngDeclareInjector({ minVersion: "12.0.0", version: "16.2.3", ngImport: i0, type: PushModule }); }
+    /** @nocollapse */ static { this.ɵfac = i0.ɵɵngDeclareFactory({ minVersion: "12.0.0", version: "17.0.0-next.8", ngImport: i0, type: PushModule, deps: [], target: i0.ɵɵFactoryTarget.NgModule }); }
+    /** @nocollapse */ static { this.ɵmod = i0.ɵɵngDeclareNgModule({ minVersion: "14.0.0", version: "17.0.0-next.8", ngImport: i0, type: PushModule, imports: [PushPipe], exports: [PushPipe] }); }
+    /** @nocollapse */ static { this.ɵinj = i0.ɵɵngDeclareInjector({ minVersion: "12.0.0", version: "17.0.0-next.8", ngImport: i0, type: PushModule }); }
 }
-i0.ɵɵngDeclareClassMetadata({ minVersion: "12.0.0", version: "16.2.3", ngImport: i0, type: PushModule, decorators: [{
+i0.ɵɵngDeclareClassMetadata({ minVersion: "12.0.0", version: "17.0.0-next.8", ngImport: i0, type: PushModule, decorators: [{
             type: NgModule,
             args: [{
                     imports: [PushPipe],
                     exports: [PushPipe],
                 }]
         }] });
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoicHVzaC5tb2R1bGUuanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyIuLi8uLi8uLi8uLi8uLi8uLi9tb2R1bGVzL2NvbXBvbmVudC9zcmMvcHVzaC9wdXNoLm1vZHVsZS50cyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiQUFBQSxPQUFPLEVBQUUsUUFBUSxFQUFFLE1BQU0sZUFBZSxDQUFDO0FBQ3pDLE9BQU8sRUFBRSxRQUFRLEVBQUUsTUFBTSxhQUFhLENBQUM7O0FBRXZDOztHQUVHO0FBS0gsTUFBTSxPQUFPLFVBQVU7aUlBQVYsVUFBVTtrSUFBVixVQUFVLFlBSFgsUUFBUSxhQUNSLFFBQVE7a0lBRVAsVUFBVTs7MkZBQVYsVUFBVTtrQkFKdEIsUUFBUTttQkFBQztvQkFDUixPQUFPLEVBQUUsQ0FBQyxRQUFRLENBQUM7b0JBQ25CLE9BQU8sRUFBRSxDQUFDLFFBQVEsQ0FBQztpQkFDcEIiLCJzb3VyY2VzQ29udGVudCI6WyJpbXBvcnQgeyBOZ01vZHVsZSB9IGZyb20gJ0Bhbmd1bGFyL2NvcmUnO1xuaW1wb3J0IHsgUHVzaFBpcGUgfSBmcm9tICcuL3B1c2gucGlwZSc7XG5cbi8qKlxuICogQGRlcHJlY2F0ZWQgVGhpcyBtb2R1bGUgaXMgZGVwcmVjYXRlZCBpbiBmYXZvciBvZiB0aGUgc3RhbmRhbG9uZSBgUHVzaFBpcGVgLlxuICovXG5ATmdNb2R1bGUoe1xuICBpbXBvcnRzOiBbUHVzaFBpcGVdLFxuICBleHBvcnRzOiBbUHVzaFBpcGVdLFxufSlcbmV4cG9ydCBjbGFzcyBQdXNoTW9kdWxlIHt9XG4iXX0=
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoicHVzaC5tb2R1bGUuanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyIuLi8uLi8uLi8uLi8uLi8uLi9tb2R1bGVzL2NvbXBvbmVudC9zcmMvcHVzaC9wdXNoLm1vZHVsZS50cyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiQUFBQSxPQUFPLEVBQUUsUUFBUSxFQUFFLE1BQU0sZUFBZSxDQUFDO0FBQ3pDLE9BQU8sRUFBRSxRQUFRLEVBQUUsTUFBTSxhQUFhLENBQUM7O0FBRXZDOztHQUVHO0FBS0gsTUFBTSxPQUFPLFVBQVU7d0lBQVYsVUFBVTt5SUFBVixVQUFVLFlBSFgsUUFBUSxhQUNSLFFBQVE7eUlBRVAsVUFBVTs7a0dBQVYsVUFBVTtrQkFKdEIsUUFBUTttQkFBQztvQkFDUixPQUFPLEVBQUUsQ0FBQyxRQUFRLENBQUM7b0JBQ25CLE9BQU8sRUFBRSxDQUFDLFFBQVEsQ0FBQztpQkFDcEIiLCJzb3VyY2VzQ29udGVudCI6WyJpbXBvcnQgeyBOZ01vZHVsZSB9IGZyb20gJ0Bhbmd1bGFyL2NvcmUnO1xuaW1wb3J0IHsgUHVzaFBpcGUgfSBmcm9tICcuL3B1c2gucGlwZSc7XG5cbi8qKlxuICogQGRlcHJlY2F0ZWQgVGhpcyBtb2R1bGUgaXMgZGVwcmVjYXRlZCBpbiBmYXZvciBvZiB0aGUgc3RhbmRhbG9uZSBgUHVzaFBpcGVgLlxuICovXG5ATmdNb2R1bGUoe1xuICBpbXBvcnRzOiBbUHVzaFBpcGVdLFxuICBleHBvcnRzOiBbUHVzaFBpcGVdLFxufSlcbmV4cG9ydCBjbGFzcyBQdXNoTW9kdWxlIHt9XG4iXX0=
